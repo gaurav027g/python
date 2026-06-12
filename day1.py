91dcd8b1b42866d81ca7e324dd73d196965722d5
Gaurav <gaurav027g@gmail.com>
2026-06-12 18:42:22 +0530

    day 1 python code

---
+++ day1.py
@@ -0,0 +1,9 @@
+# Take two numbers as input
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Calculate sum
+sum_result = a + b
+
+# Display output
+print("Sum =", sum_result)
