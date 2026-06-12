f44d7593ffe4ffa377d46124166ca79540ce2250
Gaurav <gaurav027g@gmail.com>
2026-06-12 18:58:54 +0530

    day 2 python code

---
+++ variable.py
@@ -0,0 +1,15 @@
+# 1. Integer (Whole numbers)
+age = 25
+
+# 2. Float (Decimal numbers)
+price = 19.99
+
+# 3. String (Text surrounded by quotes)
+user_name = "Alice"
+
+# 4. Boolean (True or False)
+is_logged_in = True
+
+# Printing the variables to see their value
+print(age)
+print(user_name)
