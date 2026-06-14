87072af676d40bb7af38ab873fcc4f1506a5c5e2
Gaurav <gaurav027g@gmail.com>
2026-06-14 19:38:57 +0530

    day 3 python code

---
+++ Lists,Arrays.py
@@ -0,0 +1,19 @@
+#Lists
+List1 = "sugar,salt,oil,rice,wheat"
+List2 = ["potato", "onion", "chilli", "paneer"]
+
+
+#changes
+List2[2]= "book"
+
+#Results
+R1 = List1
+R2,R3,R4,R5 = List2[0],List2[1],List2[2],List2[3] 
+R6= List2
+
+print(R1)
+print(R2)
+print(R3)
+print(R4)
+print(R5)
+print(R6)
