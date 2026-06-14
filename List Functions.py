b3e68e9db0e96017a93a2325cd0247fc4f5069da
Gaurav <gaurav027g@gmail.com>
2026-06-14 21:29:27 +0530

    day 3 python code

---
+++ List Functions.py
@@ -0,0 +1,20 @@
+#List
+List = ["potato", "onion", "chilli", "paneer"]
+List2 = [24, 43, 5675, -45, -456]
+
+#Changes
+del(List[3])
+List.append("milk")
+
+#Results
+R1 = List
+R2 = len(List)
+R3 = max(List2)
+R4 = min(List2)
+
+
+
+print(R1)
+print(R2)
+print(R3)
+print(R4)
