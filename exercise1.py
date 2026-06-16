40168b73ced6e27708081e5ef8ab9b47f018ca80
Gaurav <gaurav027g@gmail.com>
2026-06-16 22:08:42 +0530

    day 5 python code

---
+++ exercise1.py
@@ -0,0 +1,14 @@
+#Lists
+List1 = ["Aankhein Khuli", "Barbaad", "Tere Liye"]
+List2 = ["KGF1", "KGF2", "Stree2"]
+
+print(List1)
+print(List2)
+
+List3 = List1 + List2
+
+print(List3)
+
+List3.append("Mahabharat")
+
+print(List3)
