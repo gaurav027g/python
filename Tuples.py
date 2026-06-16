fc7193ba281b9417b0b76b41c02053606a679b76
Gaurav <gaurav027g@gmail.com>
2026-06-16 21:29:22 +0530

    day 5 python code

---
+++ Tuples.py
@@ -0,0 +1,14 @@
+tup1 = ("English", 35, "Physics", "History")
+
+tup1 = ("English", 35, "Physics", "History")
+
+print(tup1)
+print(tup1[0])
+print(tup1[1])
+print(tup1[2])
+print(tup1[3])
+
+del tup1
+
+print(tup1)
+    
