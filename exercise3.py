02f8846251f8e477b923f22810e0aa279f10f9c2
Gaurav <gaurav027g@gmail.com>
2026-06-16 23:27:04 +0530

    day 5 python code

---
+++ exercise3.py
@@ -0,0 +1,7 @@
+varName = "Gaurav Kumar"
+FirstName = input("Enter your first name: ")
+LastName = input("Enter your last name: ")
+
+Message = ("Hello there, %s %s")
+
+print(Message%(FirstName, LastName))
