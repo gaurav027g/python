b814f41b2e34eb7536de761e2bf9e82f12d2a4e3
Gaurav <gaurav027g@gmail.com>
2026-06-18 00:07:44 +0530

    day 6 python code

---
+++ Nested If,Else.py
@@ -0,0 +1,12 @@
+age = 14 
+
+if age > 13:
+    print("you are elligible for facebook")
+
+name = "Bob"
+if age > 13:
+    if name == "Bob":
+                print("you get special privilages")
+    else: 
+                print("you are elligible for facebook")
+        
