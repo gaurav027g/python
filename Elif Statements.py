8a644eaa704a1685c5e281307c479e7c4c4668dd
Gaurav <gaurav027g@gmail.com>
2026-06-20 22:13:41 +0530

    day 9 python code

--- Elif Statements.py
+++ Elif Statements.py
@@ -1,1 +1,15 @@
+day = "Wednesday"
 
+if day == "Monday":
+    print("its cold today")
+elif day == "Tuesday":
+   print("its hot today")
+else: 
+    print("its rainy today")
+
+if day == "Monday":
+    print("its cold today")   
+elif day == "Wednesday":
+   print("its hot today")
+else:
+   print("its rainy today")
