dcad0d52baf0fbc72e3f94fbde0c7c3d469e2dbb
Gaurav <gaurav027g@gmail.com>
2026-06-22 20:55:29 +0530

    day 11 python code

---
+++ Pass, Break and Continue.py
@@ -0,0 +1,19 @@
+number = 0
+while number < 100:
+    if number == 10:
+            break
+    print(number)
+    number = number + 1
+
+while number < 100:
+    if number == 5:
+            break
+    else:
+          pass
+    print(number)
+    number = number + 1    
+
+for p in range(0,5):
+    if p == 0:
+          continue
+    print(p)
