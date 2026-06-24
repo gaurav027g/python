fda560ef29f194f71f8e7ac1975c598d2609d123
Gaurav <gaurav027g@gmail.com>
2026-06-24 22:54:31 +0530

    day 13 python code

---
+++ exercise5.py
@@ -0,0 +1,11 @@
+day = "Tuesday"
+
+if day == "Monday":
+    print("Today is sunny")
+
+elif day == "Tuesday":
+    print("Today is sunny")
+
+else:
+    print("Today it will rain")
+
---
+++ exercise6.py
@@ -0,0 +1,6 @@
+weight = 50
+
+for p in range(0,11):
+    moonweight = weight/6
+    print(moonweight)
+    weight = weight + 1
