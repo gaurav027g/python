de1e8d7b30d4cb0ef1fee4c346cc4d3d4e90432d
Gaurav <gaurav027g@gmail.com>
2026-06-24 22:35:15 +0530

    day 13 python code

---
+++ Quick Tips.py
@@ -0,0 +1,15 @@
+string = "It's my birthday"
+print(string)
+
+string = 'It\'s my birthday'
+print(string)
+
+welcomehome = int(input("Enter first number: "))
+backtoschool = int(input("Enter second number: "))
+
+if welcomehome \
+== \
+backtoschool:
+    print("hey")
+else:
+    print("oye")
