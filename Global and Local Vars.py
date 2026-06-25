ced2739e97892da5612541240453311beabfbcff
Gaurav <gaurav027g@gmail.com>
2026-06-25 21:51:31 +0530

    day 14 python code

---
+++ Global and Local Vars.py
@@ -0,0 +1,16 @@
+numbers = 20
+
+def multiply(num1, num2):
+    numbers = num1 * num2
+    return numbers
+print(multiply(5,2))
+
+print(numbers)
+
+def multiply(num1, num2):
+    numbers1 = num1 * num2
+    return numbers1
+print(numbers1)
+
+#here you see numbers1 is not define, it only use in functions
+#so when you print(numbers1),you gets an error
