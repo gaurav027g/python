73889be3fc94295aed60a72bddc4ce7d4304fad3
Gaurav <gaurav027g@gmail.com>
2026-06-25 21:39:30 +0530

    day 14 python code

---
+++ Creating our own Functions.py
@@ -0,0 +1,9 @@
+for p in range(0,5):
+    print("hey")
+
+
+def addnumber(firstnumber,secondnumber):
+    return(firstnumber + secondnumber)
+    
+print(addnumber(int(input(":")), int(input(":"))))
+
