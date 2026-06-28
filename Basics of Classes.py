0ed56cae20f2e68b1819d6ad76635ee482123295
Gaurav <gaurav027g@gmail.com>
2026-06-28 22:33:55 +0530

    day 17 python code

--- Basics of Classes.py
+++ Basics of Classes.py
@@ -1,1 +1,9 @@
+class Students:
+    def__init__(self, name, age, grade, section):
+               self.name = name
+               self.age = age
+               self.grade = grade
+               self.section = section
 
+Student1 = Students("Bob","16","11th","B")
+print(Student1.name)
