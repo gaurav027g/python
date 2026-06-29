8b1b990d5727c6c31ef17143ebe51d4c96b7243b
Gaurav <gaurav027g@gmail.com>
2026-06-30 00:00:14 +0530

    day 17 python code

--- Basics of Classes.py
+++ Basics of Classes.py
@@ -1,9 +1,23 @@
 class Students:
-    def__init__(self, name, age, grade, section):
-               self.name = name
-               self.age = age
-               self.grade = grade
-               self.section = section
+    def __init__(self,name,age,grade,section):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.section = section
 
 Student1 = Students("Bob","16","11th","B")
 print(Student1.name)
+print(Student1.age)
+print(Student1.grade)
+print(Student1.section)
+
+student2 = Students("Avi", 14)
+
+class Students:
+      def __init__(self, name, age, grade):
+         pass
+      
+student1 = Students("Bob", 12, "7th")
+print(student1.name)
+
+# in the last its not working
