1f0dc8ae8d45256193dbec9519a536c2c07ea936
Gaurav <gaurav027g@gmail.com>
2026-06-30 23:49:21 +0530

    day 18 python code

--- Functions in Classes.py
+++ Functions in Classes.py
@@ -1,1 +1,18 @@
+class Students:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
 
+Student1 = Students("Bob", 16)
+print(Student1.name)
+print(Student1.age) 
+
+class Students:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def displayStudent(self):
+        return("Student name is " + self.name + " and age is " + str(self.age))
+    
+Student2 = Students("Bob", 16)
+print(Student2.displayStudent())
