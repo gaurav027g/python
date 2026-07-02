d9967f77cc10a528fc9bc05bcaf1e4d4d240e8af
Gaurav <gaurav027g@gmail.com>
2026-07-02 22:44:04 +0530

    day 21 python code

---
+++ Creating a File.py
@@ -0,0 +1,7 @@
+#First search "textedit" on your computer
+
+#Write someting in this and save the file 
+
+#but in my python course he say save in ".txt" but i don't know hot to save in ".txt"
+
+#I tried but i failed
