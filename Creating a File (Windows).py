08cf492932e6522b286dbe2cc5d425f2f297e734
Gaurav <gaurav027g@gmail.com>
2026-07-02 22:47:34 +0530

    day 21 python code

---
+++ Creating a File (Windows).py
@@ -0,0 +1,9 @@
+#To create a file in Windows:
+
+#1) Open up Notepad (Click the start button -> Search Notepad)
+
+#2) In Notepad, enter some text. Anything you want.
+
+#3) File -> Save, and then give the name Test
+
+#You have now successfully created your file!
