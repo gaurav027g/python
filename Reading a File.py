2cf62c7402a95f3d0b8a47f8712ff98f849aae79
Gaurav <gaurav027g@gmail.com>
2026-07-03 23:46:37 +0530

    day 22 python code

--- Reading a File.py
+++ Reading a File.py
@@ -1,1 +1,16 @@
+file1 = open("testfile.txt","r")
+print(file1.read())
 
+print(file1.read())
+
+print(file1.tell())
+
+print(file1.seek(0,0))
+
+print(file1.read())
+
+print(file1.seek(0,0))
+
+print(file1.read(22))
+print(file1.read(22))
+print(file1.read(22))
