45865dd2db81a5398e0c028e452aa2cf6a23946e
Gaurav <gaurav027g@gmail.com>
2026-07-04 21:40:56 +0530

    day 23 python code

--- Writing to a File.py
+++ Writing to a File.py
@@ -2,3 +2,13 @@ f = open("testfile.txt", "w")
 #print(f.read())
 
 print(f.write("how are you feeling today?"))
+f.close()
+f = open("testfile.txt", "r")
+print(f.read())
+
+
+n = open("testfile.txt", "w")
+print(n.write("hey i am bob"))
+
+n = open("testfile.txt", "r")
+print(n.read())
