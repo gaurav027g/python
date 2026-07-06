8cbe9361744e60ecd2606f88666cd0251436ea86
Gaurav <gaurav027g@gmail.com>
2026-07-06 22:12:31 +0530

    day 24 & 25 python code

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,1 +1,13 @@
+import random
 
+comGuess = random.randint(0,100)
+
+while True:
+    userGuess = int(input("Guess a number between 0 - 100: "))
+    if userGuess > comGuess:
+        print("Guess Lower")
+    elif userGuess < comGuess:
+        print("Guess Higher")
+    else:
+        print("Congrats, You've guessed the correct number")
+    break
