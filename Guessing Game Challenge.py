518a025c876447b0633e50ea469b39b81d7eb4fc
Gaurav <gaurav027g@gmail.com>
2026-07-07 23:58:15 +0530

    day 25 & 26 python code

--- Guessing Game Challenge.py
+++ Guessing Game Challenge.py
@@ -1,1 +1,18 @@
+guess = 50
 
+print("Think of a number between 1 and 100.")
+
+while True:
+    print("My guess is:", guess)
+
+    user = input("Enter 0 (lower), 1 (higher), 2 (correct): ")
+
+    if user == "0":
+        guess = guess - 10
+
+    elif user == "1":
+        guess = guess + 10
+
+    elif user == "2":
+        print("Yay! I guessed your number.")
+        break
