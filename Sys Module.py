87baa4b195572afbe8100d5a067ff08ba9fee002
Gaurav <gaurav027g@gmail.com>
2026-07-09 21:51:29 +0530

    day 28 python code

---
+++ Sys Module.py
@@ -0,0 +1,11 @@
+import sys
+inputStatement = sys.stdin.readline()
+print(inputStatement)
+
+inputStatement = sys.stdin.readline(10)
+print(inputStatement)
+
+sys.stdout.write("hey, how are you\n")
+
+print(sys.version)
+
