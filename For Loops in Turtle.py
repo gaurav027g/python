c93c0772f1349ce9ffc523c114e85d246f8bb286
Gaurav <gaurav027g@gmail.com>
2026-07-11 23:38:54 +0530

    day 30 python code

---
+++ For Loops in Turtle.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Pen()
+for p in range(0,8):
+    t.forward(50)
+    t.left(45)
+
+t.reset()
+for p in range(0,70):
+    t.forward(100)
+    t.left(125)
+
+t.reset()
+for p in range(0,8):
+    t.forward(100)
+    t.left(225)
