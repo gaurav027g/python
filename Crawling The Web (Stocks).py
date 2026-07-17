7029e8c9744b1b1f52d8334669da46b1fbb7032d
Gaurav <gaurav027g@gmail.com>
2026-07-17 22:58:41 +0530

    day 35 & 36 python code

--- Crawling The Web (Stocks).py
+++ Crawling The Web (Stocks).py
@@ -1,3 +1,9 @@
 import re
 import urllib.request
+#https://www.google.com/finance?q=
+url = "https://www.google.com/finance?q="
+stock = input("Enter Your Stock: ")
+url = url + stock #Concatenation of string
+print(url)
+data = urllib.request.urlopen(url).read()
 
