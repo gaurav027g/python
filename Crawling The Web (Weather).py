fe58e6ada73ce6b1891ea7921bacb7a92c1bb041
Gaurav <gaurav027g@gmail.com>
2026-07-22 21:25:33 +0530

    day 40 & 41 python code

--- Crawling The Web (Weather).py
+++ Crawling The Web (Weather).py
@@ -7,3 +7,5 @@ url = f"https://wttr.in/{city}?format=3"
 weather = requests.get(url)
 
 print(weather.text)
+
+#sir jo bataye the udemy wale wo code nahi hai ye, ye dusara code hai keuki 2026 me aisa hi code chalta hai aur sir ka 2015 ka code tha wo
