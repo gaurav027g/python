f7b8bbdd0df8fb50ff31efdc7653fe9071a47031
Gaurav <gaurav027g@gmail.com>
2026-07-26 18:52:11 +0530

    day 43 & 44 python code

--- Installing the Beautiful Soup Module.py
+++ Installing the Beautiful Soup Module.py
@@ -1,4 +1,10 @@
 #code for installing Beautiful Soup Module
 #python3 --version
+#python3 -m pip --version
+#python3 -m pip install beautifulsoup4
 
+#ye code likh ke check karo chal raha hai ya nahi
+
+#from bs4 import BeautifulSoup
+#print("Beautiful Soup Installed Successfully!")
 
