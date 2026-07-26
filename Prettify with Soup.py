6140d32ec8e16e3cce63bb2b151552a05e484f8b
Gaurav <gaurav027g@gmail.com>
2026-07-26 23:57:31 +0530

    day 44 & 45 python code

--- Prettify with Soup.py
+++ Prettify with Soup.py
@@ -1,1 +1,6 @@
-
+from bs4 import BeautifulSoup
+soup = BeautifulSoup("<html> <p> asdasdasd <strong> Hello <a > Hello </html>", "html.parser")
+print(soup)
+soup.prettify()
+print(soup.prettify())
+print(soup)
