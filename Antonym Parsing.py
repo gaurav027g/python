79ac2b0ec1d6dd26a2a755346350910d06cc52b2
Gaurav <gaurav027g@gmail.com>
2026-08-02 21:20:23 +0530

    day 51 & 52 python code

--- Antonym Parsing.py
+++ Antonym Parsing.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+data = requests.get('http://www.synonym.com/antonyms/cold/')
+soup = BeautifulSoup(data.text, 'html.parser')
+data2 = soup.find('span', {})
+data2 = soup.find('span', {'class':'equals'})
+print(data2)
+print(data2.string)
+print(data2.contents)
 
+#its not working, because the code is according to the 2015year, its not working in today's
