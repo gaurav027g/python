21c8ba90dfcced0985fcec3fccaf674389e2e6a4
Gaurav <gaurav027g@gmail.com>
2026-08-06 00:12:27 +0530

    day 56 python code

---
+++ Graph Labels.py
@@ -0,0 +1,8 @@
+import matplotlib
+import matplotlib.pyplot as pt
+pt.plot([1,2,3,4],[3,8,10,25])
+pt.title('Rain in December')
+pt.xlabel('Days in December')
+pt.ylabel('Inches of rain')
+pt.show()
+
