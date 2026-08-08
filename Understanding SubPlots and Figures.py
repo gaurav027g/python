0dd08339ef4c1fd39a32edc2d7a77c6eca39bd57
Gaurav <gaurav027g@gmail.com>
2026-08-08 23:48:11 +0530

    day 57 & 58 python code

--- Understanding SubPlots and Figures.py
+++ Understanding SubPlots and Figures.py
@@ -5,9 +5,10 @@ fig = pt.figure()
 rect = fig.patch 
 rect.set_facecolor('brown')
 
-x = [2,5,9,14]
+x = [2,4,9,14]
 y = [5,8,12,15]
 
-graph1 = fig.add_subplot(1,1,1,axisbg = 'black')
+graph1 = fig.add_subplot(1,1,1)
+graph1.set_facecolor('black')
 graph1.plot(x,y,'blue', linewidth = 2)
 pt.show()
