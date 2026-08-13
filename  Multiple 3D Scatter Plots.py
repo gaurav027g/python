f5dabd004896537b2b14a9e2f7fa4c65ade25ce0
Gaurav <gaurav027g@gmail.com>
2026-08-13 23:21:44 +0530

    day 63 python code

---
+++  Multiple 3D Scatter Plots.py
@@ -0,0 +1,22 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as pt
+import numpy as np
+
+fig = pt.figure()
+chart = fig.add_subplot(1,1,1, projection = '3d')
+
+X,Y,Z = np.array([1,2,3,4,5,6,7,8]), np.array([2,5,3,8,9,5,6,1]), np.array([3,6,2,7,5,4,5,6])
+Xx,Yy,Zz = np.array([-1,-2,-3,-4,-5,-6,-7,-8]), np.array([-2,-5,-3,-8,-9,-5,-6,-1]), np.array([-3,-6,-2,-7,-5,-4,-5,-6])
+
+X,Y = np.meshgrid(X,Y)
+
+Z = np.tile(Z, (8,1))
+
+chart.scatter(X,Y,Z, c = 'red', marker = 'o')
+chart.scatter(Xx,Yy,Zz, c = 'blue', marker = '^')
+
+chart.set_xlabel('x axis')
+chart.set_ylabel('y axis')
+chart.set_zlabel('z axis')
+
+pt.show()
