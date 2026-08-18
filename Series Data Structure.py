d3c0400aba0ce7a9ddd4871b4a64954fe6a4bc80
Gaurav <gaurav027g@gmail.com>
2026-08-18 22:20:52 +0530

    day 66,67 & 68 python code

--- Series Data Structure.py
+++ Series Data Structure.py
@@ -5,5 +5,15 @@ print(s)
 print(s[0])
 print(s[1])
 
-s = pd.Series([10, "Namaste", 23.5, "Hello"]), index = ["a", "b", "c", "d"]
+s = pd.Series([10, "Namaste", 23.5, "Hello"], index = ["a", "b", "c", "d"])
 print(s)
+
+print(s["b"])
+
+p = {"Bihar": "12.8 crore", "Uttar Pradesh": "24.3 crore", "Maharashtra": "12.9 crore"}
+cities = pd.Series(p)
+print(cities)
+
+print(cities[cities > "11 crore"])
+print(cities[cities > "15 crore"])
+print(cities[cities == "12.8 crore"])
