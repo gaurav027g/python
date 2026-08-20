52296a3436a8fc12f8e59f696146ae366bda73b5
Gaurav <gaurav027g@gmail.com>
2026-08-20 23:14:25 +0530

    day 70 python code

--- Data Frames Intro.py
+++ Data Frames Intro.py
@@ -1,1 +1,10 @@
+import pandas as pd
+data = {
+    'Students': ["Bob", "Tom", "Jack", "Ollie", "Don"],
+    'Maths': [89,56,76,64,46],
+    'Science': [87,57,37,96,65],
+    'Sports': ["Badminton", "Hockey", "Basketball", "Cricket", "Football"]
+}
 
+students = pd.DataFrame(data, columns=["Students", "Maths", "Science", "Sports"])
+print(students)
