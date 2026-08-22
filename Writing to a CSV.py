420adebe4114a79a3ebfb42664c2485ba079b9fa
Gaurav <gaurav027g@gmail.com>
2026-08-23 00:33:58 +0530

    day 72 python code

--- Writing to a CSV.py
+++ Writing to a CSV.py
@@ -1,2 +1,7 @@
 import pandas as pd
-newCSV = pd.read_csv()
+newCSV = pd.read_csv('TestData.csv', header = 0, names = ["Food", "Price", "Quantity"], usecols = [1,2,3])
+print(newCSV)
+
+newCSV.to_csv('TestData2.csv')
+newCSV = pd.read_csv('TestData2.csv')
+print(newCSV)
