1a1f47bf6b7b762d798eeb991bcdaf8f32b668ba
Gaurav <gaurav027g@gmail.com>
2026-08-22 23:55:47 +0530

    day 71 & 72 python code

--- Reading a CSV.py
+++ Reading a CSV.py
@@ -1,2 +1,12 @@
 import pandas as pd
-pd.read_csv()
+data = pd.read_csv('TestData.csv')
+print(data)
+
+data = pd.read_csv('TestData.csv', names = ["DOB", "Alphabet", "Rank1", "Rank2"])
+print(data)
+
+data = pd.read_csv('TestData.csv', names = ["DOB", "Alphabet", "Rank1", "Rank2"], header = 0)
+print(data)
+
+data = pd.read_csv('TestData.csv', usecols = [0,3])
+print(data)
