48503f4e170e85e576ac02d8dce35673b919420a
Gaurav <gaurav027g@gmail.com>
2026-08-24 00:08:56 +0530

    day 73 & 74 python code

--- Slicing Data Frames.py
+++ Slicing Data Frames.py
@@ -1,2 +1,11 @@
+import pandas as pd
+users = pd.read_csv('u.user', sep = "|", names = ["User_ID", "Age", "Gender", "Occupation", "Zip_Code"])
+print(users.head())
 
-
+print(users.tail())
+print(users.tail(2))
+print(users.head(3))
+print(users[10:15])
+print(users[:10])
+print(users[195:])
+print(users[:-1])
