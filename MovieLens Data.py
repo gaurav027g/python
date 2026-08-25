113b7ce125659bc9d9ca88d9958b670e5ab1eefd
Gaurav <gaurav027g@gmail.com>
2026-08-25 23:46:51 +0530

    day 75 python code

--- MovieLens Data.py
+++ MovieLens Data.py
@@ -3,6 +3,9 @@ col1 = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 ratings = pd.read_csv('u.data', sep = "\t", names = col1)
 print(ratings.head())
 
-col2 = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
-movies = pd.read_csv('u.item', sep = "|", names = 'col2')
+col2 = ['movie_id', 'title' 'release_date', 'video_release_date', 'imdb_url']
+movies = pd.read_csv('u.item', sep = "|", names = col2)
 print(movies.head())
+
+movie_ratings = pd.merge(movies, ratings)
+print(movie_ratings.head())
