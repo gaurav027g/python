cc89b471675a972020ef6db6c5617212c3128da7
Gaurav <gaurav027g@gmail.com>
2026-08-25 22:54:52 +0530

    day 75 python code

---
+++ MovieLens Data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+col1 = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('u.data', sep = "\t", names = col1)
+print(ratings.head())
+
+col2 = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
+movies = pd.read_csv('u.item', sep = "|", names = 'col2')
+print(movies.head())
