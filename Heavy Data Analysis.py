ef7da6f0cff7df1271b5d6911300cfc035501fde
Gaurav <gaurav027g@gmail.com>
2026-08-27 23:05:28 +0530

    day 76 & 77 python code

--- Heavy Data Analysis.py
+++ Heavy Data Analysis.py
@@ -1,1 +1,22 @@
+import pandas as pd
 
+col1 = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('u.data', sep='\t', names=col1)
+print(ratings.head())
+
+col2 = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
+movies = pd.read_csv('u.item', sep='|', names=col2)
+print(movies.head())
+
+movie_ratings = pd.merge(movies, ratings)
+print(movie_ratings.head())
+
+u_cols = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
+users = pd.read_csv('u.user', sep = '|', names = u_cols)
+print(users.head())
+
+lens = pd.merge(movie_ratings, users)
+print(lens.head())
+
+most_rated = lens.groupby('title').size0.order(ascending= False)[:20]
+print(most_rated)
