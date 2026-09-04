b38223116b13afd2b3055fa0918389f3404de32e
Gaurav <gaurav027g@gmail.com>
2026-09-04 23:59:20 +0530

    day 85 python code

---
+++ Labels.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+theLabel = Label(root, text="This is our Tkinter window")
+theLabel.pack()
+theLabel2 = Label(root, text= "This is our second sentence|" )
+theLabel2.pack()
+root.mainloop()
