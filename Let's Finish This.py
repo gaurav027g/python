361049d25b0a093011d2a14167e1111a526f1c55
Gaurav <gaurav027g@gmail.com>
2026-09-04 23:47:24 +0530

    day 82,83,84 & 85 python code

--- Let's Finish This.py
+++ Let's Finish This.py
@@ -32,3 +32,20 @@ print(movie_stats.sort_values([('rating', 'mean')], ascending = False).head())
 
 most_50 = lens.groupby('movie_id').size().sort_values(ascending = False)[:50]
 print(most_50)
+
+lens.set_index('movie_id', inplace=True)
+pivoted = lens.pivot_table(index = ['movie_id', 'title'], columns=['gender'], values='rating', fill_value=0)
+print(pivoted)
+print(pivoted.head())
+pivoted['diff'] = pivoted.M - pivoted.F
+print(pivoted.head())
+
+disagreements = pivoted[pivoted.index.get_level_values('movie_id').isin(most_50.index)]['diff']
+print(disagreements.head())
+
+import matplotlib.pyplot as plt
+plt.title('Male Vs. Female Avg. Ratings \n Where Difference > 0 = Favored by Men')
+plt.xlabel('Average Rating Difference')
+plt.ylabel('Title')
+disagreements.sort_values().plot(kind='barh', figsize = [15,10])
+plt.show()
