1d8993092c6467b3a6d106a0f1e684521ef92c88
Gaurav <gaurav027g@gmail.com>
2026-09-05 23:54:52 +0530

    day 86 python code

--- Buttons.py
+++ Buttons.py
@@ -1,1 +1,7 @@
-
+from tkinter import*
+root = Tk()
+theButton = Button(None,text = "Click Me!", fg = "Blue")
+theButton.pack()
+thButton = Button(None,text = "Hello!", fg = "Red")
+thButton.pack()
+root.mainloop()
