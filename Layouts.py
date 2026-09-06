82aca3123dc05ea6ef94a6bcd351b95d0bcabeaf
Gaurav <gaurav027g@gmail.com>
2026-09-06 13:59:49 +0530

    day 87 python code

---
+++ Layouts.py
@@ -0,0 +1,12 @@
+from tkinter import*
+root = Tk()
+
+
+
+theButton = Button(None,text = "Click Me!", fg = "Blue")
+theButton.pack()
+
+thButton = Button(None,text = "Hello!", fg = "Red")
+thButton.pack()
+
+root.mainloop()
