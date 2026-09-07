6d5216065ce9c60e800c53d80a0ee981e6d2c68a
Gaurav <gaurav027g@gmail.com>
2026-09-07 22:21:13 +0530

    day 88 python code

--- Fill.py
+++ Fill.py
@@ -1,1 +1,16 @@
+from tkinter import*
+root = Tk()
 
+Button1 = Button(None,text = "Click Me!", fg = "Blue")
+Button1.pack(side=LEFT)
+
+Button2 = Button(None,text = "Hello!", fg = "Red")
+Button2.pack()
+
+Button3 = Button(None,text = "Click Me!", fg = "Blue")
+Button3.pack(side=LEFT)
+
+Button4 = Button(None,text = "Hello!", fg = "Red")
+Button4.pack()
+
+root.mainloop()
