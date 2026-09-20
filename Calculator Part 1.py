e4e5ed31a0daf7718881f6ad44f06a01e4cb4a96
Gaurav <gaurav027g@gmail.com>
2026-09-20 23:41:30 +0530

    day 100 & 101 python code

--- Calculator Part 1.py
+++ Calculator Part 1.py
@@ -1,1 +1,9 @@
+from tkinter import*
+root = Tk()
 
+equation = StringVar()
+calculation = Label(root, textvariable=equation)
+equation.set("45+50")
+calculation.grid(columnspan=4)
+
+root.mainloop()
