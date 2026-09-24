b6520adf2b47e9e367901c34f600ca329882b2b6
Gaurav <gaurav027g@gmail.com>
2026-09-24 23:56:38 +0530

    day 103,104 & 105 python code

--- Calculator Part 3.py
+++ Calculator Part 3.py
@@ -17,42 +17,45 @@ def btnPress(num):
     equation.set(equa)
 
 Button0 = Button(root, text="0", command=lambda:btnPress(0))
-Button0.grid(row=1,column=0)
+Button0.grid(row=4,column=3)
 
 Button1 = Button(root, text="1", command=lambda:btnPress(1))
-Button1.grid(row=1,column=1)
+Button1.grid(row=1,column=0)
 
 Button2 = Button(root, text="2", command=lambda:btnPress(2))
-Button2.grid(row=1,column=2)
+Button2.grid(row=1,column=1)
 
-Button4 = Button(root, text="0", command=lambda:btnPress(0))
-Button4.grid(row=1,column=0)
+Button3 = Button(root, text="3", command=lambda:btnPress(3))
+Button3.grid(row=1,column=2)
 
-Button5 = Button(root, text="0", command=lambda:btnPress(0))
+Button4 = Button(root, text="4", command=lambda:btnPress(4))
+Button4.grid(row=1,column=2)
+
+Button5 = Button(root, text="5", command=lambda:btnPress(0))
 Button5.grid(row=1,column=0)
 
-Button6 = Button(root, text="0", command=lambda:btnPress(0))
+Button6 = Button(root, text="6", command=lambda:btnPress(0))
 Button6.grid(row=1,column=0)
 
-Button7 = Button(root, text="0", command=lambda:btnPress(0))
+Button7 = Button(root, text="7", command=lambda:btnPress(0))
 Button7.grid(row=1,column=0)
 
-Button8 = Button(root, text="0", command=lambda:btnPress(0))
+Button8 = Button(root, text="8", command=lambda:btnPress(0))
 Button8.grid(row=1,column=0)
 
-Button9 = Button(root, text="0", command=lambda:btnPress(0))
+Button9 = Button(root, text="9", command=lambda:btnPress(0))
 Button9.grid(row=1,column=0)
 
-Plus = Button(root, text="0", command=lambda:btnPress(0))
+Plus = Button(root, text="+", command=lambda:btnPress(0))
 Plus.grid(row=1,column=0)
 
-Minus = Button(root, text="0", command=lambda:btnPress(0))
+Minus = Button(root, text="-", command=lambda:btnPress(0))
 Minus.grid(row=1,column=0)
 
-Multiply = Button(root, text="0", command=lambda:btnPress(0))
+Multiply = Button(root, text="*", command=lambda:btnPress(0))
 Multiply.grid(row=1,column=0)
 
-Divide = Button(root, text="0", command=lambda:btnPress(0))
+Divide = Button(root, text="/", command=lambda:btnPress(0))
 Divide.grid(row=1,column=0)
 
 root.mainloop()
